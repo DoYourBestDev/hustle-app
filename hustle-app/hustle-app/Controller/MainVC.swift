00e5c8375b1e3e01ab4211eb9c0febd1d332e7c0
Air <mkomor@czdz.pl>
2017-11-19 16:14:12 +0100

    create project

---
+++ hustle-app/hustle-app/Controller/MainVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hustle-app
+//
+//  Created by Air on 19.11.2017.
+//  Copyright © 2017 Air. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
